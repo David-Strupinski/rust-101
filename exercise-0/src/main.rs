d4ca6053a1445f2872fc093d90eacef847e3283d
David Strupinski <david.strupinski@gmail.com>
2024-04-07 16:52:19 -0700

    try out rust fns, fmt strings

--- exercise-0/src/main.rs
+++ exercise-0/src/main.rs
@@ -1,3 +1,14 @@
 fn main() {
-    println!("Hello, world!");
+    // let mut x: u64 = 0;
+    for i in 0..20 {
+        println!("Hello, world! fib({}) is {}", i, fib(i));
+    }
+}
+
+fn fib(n: u64) -> u64 {
+    if n <= 1 {
+        n
+    } else {
+        fib(n - 1) + fib(n - 2)
+    }
 }
