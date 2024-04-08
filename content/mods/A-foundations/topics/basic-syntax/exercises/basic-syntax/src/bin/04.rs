b61da5c870f2606d251fb744cdd09a7221fb02f4
David Strupinski <david.strupinski@gmail.com>
2024-04-08 13:04:24 -0700

    complete A-4

--- content/mods/A-foundations/topics/basic-syntax/exercises/basic-syntax/src/bin/04.rs
+++ content/mods/A-foundations/topics/basic-syntax/exercises/basic-syntax/src/bin/04.rs
@@ -1,9 +1,10 @@
 fn main() {
-    for (n in [10, 20, 30, 40]) {
+    for n in [10, 20, 30, 40] {
         let mult = if n < 25 {
             n * 4
         } else {
-            n * 3;
+            // ending in semicolon makes it an expression, which returns a unit
+            n * 3
         };
         println!("{}", mult);
     }
