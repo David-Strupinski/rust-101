9a542856d0431b8d2bfedcd2af9769a170d3a548
David Strupinski <david.strupinski@gmail.com>
2024-04-08 12:37:42 -0700

    redo fn loop, write test

--- content/mods/A-foundations/topics/basic-syntax/exercises/basic-syntax/src/bin/03.rs
+++ content/mods/A-foundations/topics/basic-syntax/exercises/basic-syntax/src/bin/03.rs
@@ -1,17 +1,35 @@
 fn main() {
     let input = [23, 82, 16, 45, 21, 94, 12, 34];
 
+    let (largest, smallest) = find_largest_and_smallest(input);
+
+    println!("{} is largest and {} is smallest", largest, smallest);
+}
+
+fn find_largest_and_smallest(input: [i32; 8]) -> (i32, i32) {
     let mut largest = input[0];
     let mut smallest = input[0];
 
-    for &item in input.iter() {
-        if item > largest {
-            largest = item;
+    for &n in &input {
+        if n > largest {
+            largest = n;
         }
-        if item < smallest {
-            smallest = item;
+        if n < smallest {
+            smallest = n;
         }
     }
 
-    println!("{} is largest and {} is smallest", largest, smallest);
+    (largest, smallest)
+}
+
+#[cfg(test)]
+mod test {
+    use super::*;
+
+    #[test]
+    fn test_find_largest_and_smallest() {
+        assert_eq!(find_largest_and_smallest([23, 82, 16, 45, 21, 94, 12, 34]), (94, 12));
+        assert_eq!(find_largest_and_smallest([1, 2, 3, 4, 5, 6, 7, 8]), (8, 1));
+        assert_eq!(find_largest_and_smallest([1, 2, 3, 4, 5, 6, 7, 8]), (8, 1));
+    }
 }
