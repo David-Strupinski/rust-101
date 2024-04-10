33802abea8c0e2378a7fbe4503158099f29fcec3
David Strupinski <david.strupinski@gmail.com>
2024-04-10 00:21:33 -0700

    solve error-handling exercise

--- content/mods/A-foundations/topics/optionals-errors/exercises/error-handling/src/main.rs
+++ content/mods/A-foundations/topics/optionals-errors/exercises/error-handling/src/main.rs
@@ -1,6 +1,5 @@
 /// While taking a first stab at programs, using panic!() is a quick-and-dirty way to do error handling; but panic!() has the obvious drawback
 /// that it is all-or-nothing: you cannot recover from it (in general).
-
 // Consider this "interactive hello world" (that is a bit fussy about what is a valid name), where the intent is that the program repeats
 // the question if the user entered an invalid name.
 //
@@ -20,35 +19,55 @@
 //
 // NOTE: You will (hopefully) discover that "?" doesn't work in this context, and the resulting code
 // is a bit explicit about the errors --- we can solve that with traits, next week!
-
-use std::io::{BufRead, self, Write};
+use std::io::{self, BufRead, Write};
 
 #[derive(Debug)]
-enum MyError{ InvalidName,IOError( io::Error),
+enum MyError {
+    InvalidName,
+    IOError(io::Error),
 }
 
-fn get_username( )
-->  String
-{
+fn get_username() -> Result<String, MyError> {
     print!("Username: ");
-    io::stdout().flush();
 
-    let mut input=String::new();
-    io::stdin().lock().read_line(&mut input); input=input.trim().to_string();
-
-    for c in input.chars()
-    {
-	if !char::is_alphabetic(c) { panic!("that's not a valid name, try again"); }
+    let result = io::stdout().flush();
+    if let Err(e) = result {
+        panic!("failed to flush stdout: {}", e);
     }
 
-if input.is_empty() {
-panic!("that's not a valid name, try again");
-}
+    let mut input = String::new();
 
-    input
+    let result = io::stdin().lock().read_line(&mut input);
+    if let Err(e) = result {
+        return Result::Err(MyError::IOError(e));
+    }
+
+    input = input.trim().to_string();
+
+    for c in input.chars() {
+        if !char::is_alphabetic(c) {
+            return Result::Err(MyError::InvalidName);
+        }
+    }
+
+    if input.is_empty() {
+        return Result::Err(MyError::InvalidName);
+    }
+
+    Result::Ok(input)
 }
 
 fn main() {
-    let name=get_username();
-    println!("Hello {name}!")
+    loop {
+        let result = get_username();
+        if let Err(MyError::InvalidName) = result {
+            println!("That's not a valid name, try again");
+        } else if let Err(MyError::IOError(e)) = result {
+            panic!("failed to read line: {}", e);
+        } else {
+            let name = result.unwrap();
+            println!("Hello {}!", name);
+            break;
+        };
+    }
 }
